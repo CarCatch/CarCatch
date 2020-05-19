2e99f0d5d2a9334810f4e8ccce5cddf02e82e924
CarCatch <65573209+CarCatch@users.noreply.github.com>
2020-05-18 20:59:21 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,19 @@
+﻿<?php
+    if(isset($_POST['submit'])){
+        $name=$_POST['name'];
+        $email=$_POST['email'];
+        $province=$_POST['province'];
+        $msg=$_POST['msg'];
+
+        $to='carcatchinc@gmail.com';
+        $subject='Contact form submission';
+        $message="Name: ".$name."\n"."Email: ".$email."\n"."Province: ".$province."\n". "Wrote the following: "."\n\n".$msg;
+        $headers="From: ".$email;
+
+    if(mail($to, $subject, $message, $headers)){
+     echo "<h1> Sent Successfully! Thank you for your message.";
+    }
+    else{
+        echp "Something went wrong!";
+    }
+    }
